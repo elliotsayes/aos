5ce7a0357759e4e131e2eefeeed71fcbbee59c32
Elliot Sayes <elliot@sayes.cloud>
2024-03-23 02:55:29 +0800

    demo

---
+++ process/test/sqlite_1.test.js
@@ -0,0 +1,61 @@
+import { test } from 'node:test'
+import * as assert from 'node:assert'
+import AoLoader from './load.cjs'
+import fs from 'fs'
+
+const env = {
+  Process: {
+    Id: 'AOS',
+    Owner: 'FOOBAR',
+    Tags: [
+      { name: 'Name', value: 'Thomas' }
+    ]
+  }
+}
+const msg = (cmd) => ({
+  Target: 'AOS',
+  Owner: 'FOOBAR',
+  ['Block-Height']: "1000",
+  Id: "1234xyxfoo",
+  Module: "WOOPAWOOPA",
+  Tags: [
+    { name: 'Action', value: 'Run' }
+  ],
+  Data: cmd
+})
+
+const wasm = fs.readFileSync('./process.wasm')
+
+test('create sqlite db, run insert & select', async () => {
+  const handle = await AoLoader(wasm)
+
+  const run1 = `
+local sqlite3 = require("lsqlite3")
+
+db = sqlite3.open_memory()
+
+db:exec[[
+  CREATE TABLE test (id INTEGER PRIMARY KEY, content);
+  INSERT INTO test VALUES (NULL, 'Hello Lua');
+  INSERT INTO test VALUES (NULL, 'Hello Sqlite3');
+  INSERT INTO test VALUES (NULL, 'Hello ao!!!');
+]]
+return "ok"
+`
+  const result1 = await handle(null, msg(run1), env)
+  console.log("result1:\n" + result1.Output?.data.output)
+  assert.equal(result1.Output?.data.output, "ok")
+
+  const run2 = `
+local s = ""
+
+for row in db:nrows("SELECT * FROM test") do
+  s = s .. row.id .. ": " .. row.content .. "\\n"
+end
+
+return s
+`
+  const result2 = await handle(result1.Memory, msg(run2), env)
+  console.log("\nresult2:\n" + result2.Output?.data.output)
+  assert.equal(result2.Output?.data.output, "1: Hello Lua\n2: Hello Sqlite3\n3: Hello ao!!!\n")
+})
--- process/test/sqlite_2.test.js
+++ process/test/sqlite_2.test.js
@@ -3,30 +3,32 @@ import * as assert from 'node:assert'
 import AoLoader from './load.cjs'
 import fs from 'fs'
 
+const env = {
+  Process: {
+    Id: 'AOS',
+    Owner: 'FOOBAR',
+    Tags: [
+      { name: 'Name', value: 'Thomas' }
+    ]
+  }
+}
+const msg = (cmd) => ({
+  Target: 'AOS',
+  Owner: 'FOOBAR',
+  ['Block-Height']: "1000",
+  Id: "1234xyxfoo",
+  Module: "WOOPAWOOPA",
+  Tags: [
+    { name: 'Action', value: 'Run' }
+  ],
+  Data: cmd
+})
+
 const wasm = fs.readFileSync('./process.wasm')
 
 test('create sqlite db, run insert & select', async () => {
   const handle = await AoLoader(wasm)
-  const env = {
-    Process: {
-      Id: 'AOS',
-      Owner: 'FOOBAR',
-      Tags: [
-        { name: 'Name', value: 'Thomas' }
-      ]
-    }
-  }
-  const msg = (cmd) => ({
-    Target: 'AOS',
-    Owner: 'FOOBAR',
-    ['Block-Height']: "1000",
-    Id: "1234xyxfoo",
-    Module: "WOOPAWOOPA",
-    Tags: [
-      { name: 'Action', value: 'Run' }
-    ],
-    Data: cmd
-  })
+
   const run1 = `
 sqlite3 = require("lsqlite3")
 db = sqlite3.open_memory()
@@ -36,9 +38,10 @@ db:exec "INSERT INTO test VALUES (2, 4)"
 db:exec "INSERT INTO test VALUES (3, 6)"
 db:exec "INSERT INTO test VALUES (4, 8)"
 db:exec "INSERT INTO test VALUES (5, 10)"
-return "ok"`
+return "ok"
+`
   const result1 = await handle(null, msg(run1), env)
-  console.log(result1.Output?.data.output)
+  console.log("result1:\n" + result1.Output?.data.output)
   assert.equal(result1.Output?.data.output, "ok")
 
   const run2 = `
@@ -49,8 +52,9 @@ local s = ""
 for col1, col2, sum in db:urows("SELECT *, my_sum(col1, col2) FROM test") do
   s = s .. col1 .. " + " .. col2 .. " = " .. sum .. "\\n"
 end
-return s`
+return s
+`
   const result2 = await handle(result1.Memory, msg(run2), env)
-  console.log(result2.Output?.data.output)
+  console.log("\nresult2:\n" + result2.Output?.data.output)
   assert.equal(result2.Output?.data.output, "1 + 2 = 3.0\n2 + 4 = 6.0\n3 + 6 = 9.0\n4 + 8 = 12.0\n5 + 10 = 15.0\n")
 })
